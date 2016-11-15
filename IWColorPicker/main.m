fe3105cbd638a1c9731c6fadc933b0738e79c950
Marek Hrusovsky <xhruso00@gmail.com>
2016-11-15 17:11:56 +0100

    Initial Commit

---
+++ IWColorPicker/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  IWColorPicker
+//
+//  Created by Marek Hrusovsky on 11/15/16.
+//  Copyright © 2016 Marek Hrusovsky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
